9f3dda11516f9e8aa3276e73ea8f50b1ab8f3ab3
jeswaa <jeswaaa1803@gmail.com>
2025-02-08 19:57:26 +0800

    GoogleAuth

--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
     ],
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'client_secret' => env('GOOGLE_CLIENT_PRIVATE'),
         'redirect' => env('GOOGLE_REDIRECT')
     ]
 
